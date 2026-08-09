d7d06bf2803fb56bc05825a84b410fcca66d660a
Ashutosh Kumar <161447790+aashutosh-ks@users.noreply.github.com>
2026-08-09 17:01:27 +0530

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,13 @@
+
+import model.ElectricityBill;
+import model.UserData;
+import ui.LoginFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class MainApp {
+    public static void main(String[] args) {
+
+        new LoginFrame();
+    }
+}
